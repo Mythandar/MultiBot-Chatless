2498b21cb957e196b5476566d12eb70c50162774
Mythandar <ennok.t@gmail.com>
2026-07-25 21:36:03 -0600

    Show silver and copper in bot inventory

---
+++ Features/MultiBotInventoryMoney.lua
@@ -0,0 +1,64 @@
+if not MultiBot then return end
+
+local function formatInventoryMoney(summary)
+    summary = summary or {}
+    local gold = tonumber(summary.gold) or 0
+    local silver = tonumber(summary.silver) or 0
+    local copper = tonumber(summary.copper) or 0
+    local label = MultiBot.L("info.inventory.money_label", "Money")
+
+    return string.format(
+        "|cffffff00%s:|r %d|cffffd700g|r %d|cffc7c7cfs|r %d|cffeda55fc|r",
+        label,
+        gold,
+        silver,
+        copper
+    )
+end
+
+local function refreshInventoryMoney(inventory)
+    if not inventory or not inventory.moneyLabel then
+        return
+    end
+
+    inventory.moneyLabel:SetText(formatInventoryMoney(inventory.summary))
+end
+
+local function wrapInventoryMoney(inventory)
+    if not inventory or inventory._mbMoneyDisplayWrapped then
+        return inventory
+    end
+
+    inventory._mbMoneyDisplayWrapped = true
+
+    local originalBeginPayload = inventory.beginPayload
+    inventory.beginPayload = function(self, ...)
+        local result = originalBeginPayload and originalBeginPayload(self, ...)
+        refreshInventoryMoney(self)
+        return result
+    end
+
+    local originalApplySummaryLine = inventory.applySummaryLine
+    inventory.applySummaryLine = function(self, ...)
+        local result = originalApplySummaryLine and originalApplySummaryLine(self, ...)
+        refreshInventoryMoney(self)
+        return result
+    end
+
+    local originalApplySummaryData = inventory.applySummaryData
+    inventory.applySummaryData = function(self, ...)
+        local result = originalApplySummaryData and originalApplySummaryData(self, ...)
+        refreshInventoryMoney(self)
+        return result
+    end
+
+    refreshInventoryMoney(inventory)
+    return inventory
+end
+
+local originalInitializeInventoryFrame = MultiBot.InitializeInventoryFrame
+if type(originalInitializeInventoryFrame) == "function" then
+    MultiBot.InitializeInventoryFrame = function(...)
+        return wrapInventoryMoney(originalInitializeInventoryFrame(...))
+    end
+end
